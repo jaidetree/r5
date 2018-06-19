17156e085bf67e298dc56886cee59034e806b7a8
Jay Zawrotny <jayzawrotny@gmail.com>
2018-06-19 18:24:32 -0400

    Made task lazy loading more clear and consistent

--- tasks/index.js
+++ tasks/index.js
@@ -1,13 +1,13 @@
-import * as R from 'ramda';
+import { prop, unary } from 'ramda';
 import { argv } from 'yargs';
-import HStream from 'highland';
+import Stream from 'highland';
 
 
 // Get the gulp tasks from argv._ and require them
 // this way we are only loading the task files as needed
 
 argv
-  |> R.prop('_')
-  |> HStream
-  |> HStream.map(taskName => `./${taskName}/${taskName}.task.js`)
-  |> HStream.each(R.unary(require));
+  |> Stream.of
+  |> Stream.flatMap(prop('_'))
+  |> Stream.map(taskName => `./${taskName}/${taskName}.task.js`)
+  |> Stream.each(unary(require));
