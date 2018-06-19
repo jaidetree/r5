2d35c315504c2c44c3fef0579f0145db4434e9d0
Jay Zawrotny <jayzawrotny@gmail.com>
2018-06-19 18:22:38 -0400

    Updated task loading to make it lazier

--- tasks/index.js
+++ tasks/index.js
@@ -1,3 +1,13 @@
-import './build';
-import './serve';
-import './test';
+import * as R from 'ramda';
+import { argv } from 'yargs';
+import HStream from 'highland';
+
+
+// Get the gulp tasks from argv._ and require them
+// this way we are only loading the task files as needed
+
+argv
+  |> R.prop('_')
+  |> HStream
+  |> HStream.map(taskName => `./${taskName}/${taskName}.task.js`)
+  |> HStream.each(R.unary(require));
